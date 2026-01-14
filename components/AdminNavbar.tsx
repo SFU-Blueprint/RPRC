e3d03d676b619a4510f424b20e18ad4016de54b9
Poorvi <poorvi.b26@gmail.com>
2026-01-13 22:14:13 -0500

    fix: update sizing of navbar for responsiveness

--- components/AdminNavbar.tsx
+++ components/AdminNavbar.tsx
@@ -6,27 +6,51 @@ type AdminNavbarProps = {
 };
 
 export default function AdminNavbar({
-  adminName = 'Admin Name',
+  adminName,
   profilePictureUrl,
 }: AdminNavbarProps) {
   return (
-    <div className="h-34.5 bg-[#393533]">
-      <div className="flex items-center h-full justify-between ml-17.5 mr-17.5">
+    <div className="sticky top-0 z-50 w-full bg-[#393533]">
+      <div
+        className="
+          mx-auto
+          max-w-[120rem]
+          min-w-[30rem]
+          px-6 md:px-10 xl:px-16
+          h-[8.75rem]
+          flex items-center justify-between
+        "
+      >
         <p
-          className={`font-black text-[#89CC62] text-[48px] leading-[90%] ${robotoCondensed.className}`}
+          className={`
+            ${robotoCondensed.className}
+            font-black text-[#89CC62]
+            text-[2rem] sm:text-[2.5rem] xl:text-[3rem]
+            leading-[90%]
+          `}
         >
           Richmond Poverty <br />
           Reduction Coalition
         </p>
-        <div className="flex items-center gap-x-4">
+
+        <div className="flex items-center gap-4">
           <p
-            className={`${inter.className} text-[#89CC62] font-semibold text-[32px]`}
+            className={`
+              ${inter.className}
+              text-[#89CC62]
+              font-semibold
+              text-[1.25rem] sm:text-[1.5rem] xl:text-[2rem]
+            `}
           >
             {adminName}
           </p>
+
           {profilePictureUrl ? (
-            // eslint-disable-next-line @next/next/no-img-element
-            <img src={profilePictureUrl} alt="" className="h-22.5 w-22.5" />
+            <img
+              src={profilePictureUrl}
+              alt=""
+              className="h-22.5 w-22.5 rounded-full object-cover"
+            />
           ) : (
             <div className="h-22.5 w-22.5 rounded-full bg-[#7C7C7C]" />
           )}
