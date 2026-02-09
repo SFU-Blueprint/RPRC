8107b08c0a9d3224fb43ee4859d01f600397a2b7
Mark Vu <mark.vu@agilebits.com>
2026-02-08 21:50:25 -0800

    restyle and refactor membership to homepage

--- app/admin/dashboard/[id]/SubmitReview.tsx
+++ app/admin/dashboard/[id]/SubmitReview.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { inter, robotoCondensed } from '@/app/fonts';
-import { Input } from '@/components/ui/Input';
+import { Input } from '@/components/ui/input';
 import { RadioGroup } from '@/components/RadioGroup';
 import { useState } from 'react';
 import { Button } from '@/components/ui/Button';
